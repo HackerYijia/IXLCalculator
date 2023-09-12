dd0c558396696d2cd3393028f06869434d560f71
诺影科技 NoxShadow Tech <115096818+HackerYijia@users.noreply.github.com>
2023-09-12 14:49:50 -0400

    Create Use-This-In-Production.js

---
+++ Use-This-In-Production.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+  var s=document.createElement('script');
+  s.src='https://raw.githubusercontent.com/HackerYijia/IXLCalculator/main/dev.js';
+  document.body.appendChild(s);
+})()
